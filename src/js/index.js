9c273c0230c46744f9dab9a6cd961cb6716a50f7
brunotxrs <elielsonbrunno@gmail.com>
2025-02-09 16:39:09 -0400

    feat: complete the interaction of artists and area music !

--- src/js/index.js
+++ src/js/index.js
@@ -7,19 +7,7 @@ let xmarkSolid = document.getElementById('xmark-solid');
 let headerNav = document.getElementById('header_nav')
 
 barsSolid.addEventListener('click', () => {
-// deixa a area de musica oculta
-    artistaOneMusic.style.display = 'none';
-
-    // area de artista fica oculta se estiver visivel
-    if(artistasPopularesBoxOne.style.display === ''){
-
-        artistasPopularesBoxOne.style.display = 'none';
-        // quando clicar no artista a area da musica fica visivel
-        artistasPopularesBoxOne.addEventListener('click', () => {
-            artistasPopularesBoxOne.style.display = 'none';
-            artistaOneMusic.style.display = 'block';
-        })
-    }
+    artistasPopularesBoxOne.style.display = 'none';
     barsSolid.style.display = 'none';
     xmarkSolid.style.display = 'block';
     logo.style.opacity = '0';
@@ -28,7 +16,33 @@ barsSolid.addEventListener('click', () => {
     headerNav.style.display = 'block';
     premium.style.display = 'none';
 
+// deixa a area de musica oculta
+    artistMusic.style.display = 'none';
+    
+
+    // area de artista fica oculta se estiver visivel
+    if(artistasPopularesBoxOne.style.display === ''){
+
+        artistasPopularesBoxOne.style.display = 'none';
+        // quando clicar no artista a area da musica fica visivel
+        artistasPopularesBoxOne.addEventListener('click', () => {
+            artistasPopularesBoxOne.style.display = 'none';
+        })
+    }
+
+    
+      
+    
+
 })
+
+// função redireciona para a página principal
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Backspace") {
+        window.location.replace("index.html"); 
+    }
+});
+
 
 xmarkSolid.addEventListener('click', () => {
 // area do artista estiver oculta ela ficara visivel
@@ -60,10 +74,245 @@ fechar.addEventListener('click', () => {
 
 // -----------area do artistas------------
 let artistasPopularesBoxOne = document.getElementById('artistas_populares_box_one');
-let artistaOne = document.getElementById('artista_one');
-let artistaOneMusic = document.getElementById('artista_one_music');
+let play;
+let artista;
+let artistMusic;
 
-artistaOne.addEventListener('click', () => {
-    artistasPopularesBoxOne.style.display = 'none';
-    artistaOneMusic.style.display = 'block';
+artista = document.getElementById('art-1');
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-1');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-1');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+artista = document.getElementById('art-2');
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-2');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-2');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+artista = document.getElementById('art-3');
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-3');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-3');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+artista = document.getElementById('art-4')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-4');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-4');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-5')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-5');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-5');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-6')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-6');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-6');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+
+artista = document.getElementById('art-7')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-7');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-7');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-8')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-8');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-8');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-9')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-9');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-9');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-10')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-10');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-10');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-11')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-11');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-11');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-12')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-12');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-12');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-13')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-13');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-13');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-14')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-14');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-14');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-15')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-15');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-15');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-16')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-16');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-16');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+
+artista = document.getElementById('art-17')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-17');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-17');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-18')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-18');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-18');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-19')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-19');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-19');
+        artistMusic.style.display = 'block';
+    })
+    
+})
+
+artista = document.getElementById('art-20')
+artista.addEventListener('click', () => {
+    play = document.getElementById('play-20');
+    play.style.opacity = '1';
+    play.addEventListener('click', () => {
+        artistasPopularesBoxOne.style.display = 'none';
+        artistMusic = document.getElementById('artist-20');
+        artistMusic.style.display = 'block';
+    })
+    
 })
