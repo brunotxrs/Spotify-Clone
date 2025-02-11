f3e1db1ed8769087cde337f0653a160e80f3c14f
brunotxrs <elielsonbrunno@gmail.com>
2025-02-11 17:19:33 -0400

    feat: update the interaction new structure to code more clean!

--- src/js/index.js
+++ src/js/index.js
@@ -1,302 +1,98 @@
-// area do header
-let logo = document.getElementById('logo');
-let search = document.getElementById('search');
-let button = document.getElementById('button');
-let barsSolid = document.getElementById('bars-solid');
-let xmarkSolid = document.getElementById('xmark-solid');
-let headerNav = document.getElementById('header_nav');
-let headerContainer = document.getElementById('header_container');
+var barsSolid = document.getElementById('bars-solid');
+var xmarkSolid = document.getElementById('xmark-solid');
+var logo = document.getElementById('logo');
+var logo1 = document.getElementById('logo-1')
+var search = document.getElementById('search');
+var search1 = document.getElementById('search-1');
+var button = document.getElementById('button');
+var button1 = document.getElementById('button-1');
+var premium = document.getElementById('premium');
+var sectionArtist = document.getElementById('section_artist');
+var sectionAlbuns = document.getElementById('section_albuns');
+var headerNav = document.getElementById('header_nav');
 
-let single = document.getElementById('single')
+// -------------função do Nav-----------------
+function visivelNav(show) {
 
-barsSolid.addEventListener('click', () => {
-    headerContainer.style.justifyContent = 'right'
-    artistasPopularesBoxOne.classList.add('hidden');
-    barsSolid.classList.add('hidden');
-    xmarkSolid.style.marginRight = '20px'
-    xmarkSolid.classList.remove('hidden');
-    logo.classList.add('hidden');
-    search.classList.add('hidden');
-    button.classList.add('hidden');
-    headerNav.classList.remove('hidden');
-    premium.classList.add('hidden');
-    single.classList.add('hidden');
-
-// deixa a area de musica oculta
-    artistMusic.style.display = 'none';
-
-    
-
-})
-
-xmarkSolid.addEventListener('click', () => {
-    artistasPopularesBoxOne.classList.remove('hidden');
-    headerContainer.style.justifyContent = 'space-evenly';
-    barsSolid.classList.remove('hidden')
-    xmarkSolid.classList.add('hidden');
-    logo.classList.remove('hidden');
-    search.classList.remove('hidden');
-    button.classList.remove('hidden');
-    headerNav.classList.add('hidden');
-    premium.classList.remove('hidden');
-    single.classList.remove('hidden');
-
-    artistMusic.style.display = 'block';
-
-})
-
-document.addEventListener("keydown", function (event) {
-    if (event.key === "Backspace" && document.activeElement.tagName !== "INPUT") {
-        window.location.replace("index.html"); 
+    if(show){
+        headerNav.classList.remove('hidden');
+        xmarkSolid.classList.remove('hidden');
+        logo.style.opacity = '0';
+        logo1.classList.add('curso-auto');
+        search.style.opacity = '0';
+        search1.classList.add('curso-auto');
+        button.style.opacity = '0';
+        button1.classList.add('curso-auto');
+        barsSolid.classList.add('hidden');
+        premium.classList.add('hidden');
+        sectionArtist.classList.add('hidden');
+        sectionAlbuns.classList.add('hidden');
+        return;
+    } else {
+        headerNav.classList.add('hidden');
+        xmarkSolid.classList.add('hidden');
+        logo.style.opacity = '1';
+        logo1.classList.remove('curso-auto');
+        search.style.opacity = '1';
+        search1.classList.remove('curso-auto');
+        button.style.opacity = '1';
+        button1.classList.remove('curso-auto');
+        barsSolid.classList.remove('hidden');
+        premium.classList.remove('hidden');
+        sectionArtist.classList.remove('hidden');
+        sectionAlbuns.classList.remove('hidden');
     }
-});
+    
+}
 
+barsSolid.addEventListener('click', () => visivelNav(true));
+xmarkSolid.addEventListener('click', () => visivelNav(false));
 
-// --------- area da mensagem Seja Premium
+// ------------------------------------------------------------------
 
-let premium = document.getElementById('premium');
-let fechar = document.getElementById('fechar');
+// ------- area do Premium --------
+var fechar = document.getElementById('fechar');
 fechar.addEventListener('click', () => {
-    premium.style.display = 'none';
+    premium.classList.add('hidden');
 })
 
-// ---------------------------------------
+//------------------------------------------
 
-// -----------area do artistas------------
-let artistasPopularesBoxOne = document.getElementById('artistas_populares_box_one');
-let play;
-let artista;
-let artistMusic;
+// -------------- area dos artistas ------------------
+var artistBox = document.getElementById('artist_box');
+var play1 = document.getElementById('play-1');
+var artist1 = document.getElementById('artist-1')
+play1.addEventListener('click', () => {
+    artistBox.classList.add('hidden');
+    artist1.classList.remove('hidden');    
+})
 
+var play2 = document.getElementById('play-2');
+var artist2 = document.getElementById('artist-2')
+play2.addEventListener('click', () => {
+    artistBox.classList.add('hidden');
+    artist2.classList.remove('hidden');    
+})
 
+var play3 = document.getElementById('play-3');
+var artist3 = document.getElementById('artist-3')
+play3.addEventListener('click', () => {
+    artistBox.classList.add('hidden');
+    artist3.classList.remove('hidden');    
+})
 
-artista = document.getElementById('art-1');
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-1');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-1');
-        artistMusic.classList.remove('hidden');
-    })
+//-------area da Musica Albuns----------------
+var artistMusicBox = document.getElementById('artist-music-box')
+var play21 = document.getElementById('play-21');
+var album1 = document.getElementById('album-1')
+var single = document.getElementById('single')
+var musicAlbuns = document.getElementById('music_albuns')
+var album1 = document.getElementById('album-1')
+play21.addEventListener('click',  () => {
+    artistBox.classList.add('hidden');
+    artistMusicBox.classList.add('hidden');
+    single.classList.add('hidden');
+    musicAlbuns.classList.remove('hidden');
+    album1.classList.remove('hidden');
     
-})
-artista = document.getElementById('art-2');
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-2');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-2');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-artista = document.getElementById('art-3');
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-3');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-3');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-artista = document.getElementById('art-4')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-4');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-4');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-5')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-5');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-5');
-        artistMusic.classList.remove('hidden');
-        
-    })
-    
-})
-
-artista = document.getElementById('art-6')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-6');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-6');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-
-artista = document.getElementById('art-7')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-7');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-7');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-8')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-8');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-8');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-9')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-9');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-9');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-10')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-10');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-10');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-11')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-11');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-11');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-12')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-12');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-12');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-13')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-13');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-13');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-14')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-14');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-14');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-15')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-15');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-15');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-16')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-16');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-16');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-
-artista = document.getElementById('art-17')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-17');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-17');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-18')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-18');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-18');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-19')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-19');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-19');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-artista = document.getElementById('art-20')
-artista.addEventListener('click', () => {
-    play = document.getElementById('play-20');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        artistMusic = document.getElementById('artist-20');
-        artistMusic.classList.remove('hidden');
-    })
-    
-})
-
-
-let boxAlbuns1 = document.getElementById('box_albuns-1');
-boxAlbuns1.addEventListener('click', () => {
-    play = document.getElementById('play-21');
-    play.addEventListener('click', () => {
-        artistasPopularesBoxOne.style.display = 'none';
-        single.style.display = 'none';
-    })
-
 })
