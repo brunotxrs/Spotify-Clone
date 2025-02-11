66a498cff04b1436e139e558fe591cd48554f44f
brunotxrs <elielsonbrunno@gmail.com>
2025-02-11 18:37:03 -0400

    feat: change of var to const and  change the functions of area artist to function forEach and create more one interaction to area album !

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
-var barsSolid = document.getElementById('bars-solid');
-var xmarkSolid = document.getElementById('xmark-solid');
-var logo = document.getElementById('logo');
-var logo1 = document.getElementById('logo-1')
-var search = document.getElementById('search');
-var search1 = document.getElementById('search-1');
-var button = document.getElementById('button');
-var button1 = document.getElementById('button-1');
-var premium = document.getElementById('premium');
-var sectionArtist = document.getElementById('section_artist');
-var sectionAlbuns = document.getElementById('section_albuns');
-var headerNav = document.getElementById('header_nav');
+const barsSolid = document.getElementById('bars-solid');
+const xmarkSolid = document.getElementById('xmark-solid');
+const logo = document.getElementById('logo');
+const logo1 = document.getElementById('logo-1')
+const search = document.getElementById('search');
+const search1 = document.getElementById('search-1');
+const button = document.getElementById('button');
+const button1 = document.getElementById('button-1');
+const premium = document.getElementById('premium');
+const sectionArtist = document.getElementById('section_artist');
+const sectionAlbuns = document.getElementById('section_albuns');
+const headerNav = document.getElementById('header_nav');
 
 // -------------função do Nav-----------------
 function visivelNav(show) {
@@ -51,48 +51,55 @@ xmarkSolid.addEventListener('click', () => visivelNav(false));
 // ------------------------------------------------------------------
 
 // ------- area do Premium --------
-var fechar = document.getElementById('fechar');
+const fechar = document.getElementById('fechar');
 fechar.addEventListener('click', () => {
     premium.classList.add('hidden');
 })
 
 //------------------------------------------
 
-// -------------- area dos artistas ------------------
-var artistBox = document.getElementById('artist_box');
-var play1 = document.getElementById('play-1');
-var artist1 = document.getElementById('artist-1')
-play1.addEventListener('click', () => {
-    artistBox.classList.add('hidden');
-    artist1.classList.remove('hidden');    
-})
+const artistBox = document.getElementById('artist_box');
+const playButtons = document.querySelectorAll('[id^="play-"]');
 
-var play2 = document.getElementById('play-2');
-var artist2 = document.getElementById('artist-2')
-play2.addEventListener('click', () => {
-    artistBox.classList.add('hidden');
-    artist2.classList.remove('hidden');    
-})
+playButtons.forEach(play => {
+    play.addEventListener('click', () => {
+        artistBox.classList.add('hidden');
 
-var play3 = document.getElementById('play-3');
-var artist3 = document.getElementById('artist-3')
-play3.addEventListener('click', () => {
-    artistBox.classList.add('hidden');
-    artist3.classList.remove('hidden');    
-})
+        const artistId = play.id.replace('play-', 'artist-');
+        const artist = document.getElementById(artistId);
+        
+        if (artist) {
+            artist.classList.remove('hidden');
+        }
+    });
+});
+
 
 //-------area da Musica Albuns----------------
+
 var artistMusicBox = document.getElementById('artist-music-box')
-var play21 = document.getElementById('play-21');
-var album1 = document.getElementById('album-1')
+
 var single = document.getElementById('single')
 var musicAlbuns = document.getElementById('music_albuns')
-var album1 = document.getElementById('album-1')
+
+const play21 = document.getElementById('play-21');
+const album1 = document.getElementById('album-1')
 play21.addEventListener('click',  () => {
     artistBox.classList.add('hidden');
     artistMusicBox.classList.add('hidden');
     single.classList.add('hidden');
     musicAlbuns.classList.remove('hidden');
     album1.classList.remove('hidden');
-    
+  
+})
+
+const play22 = document.getElementById('play-22');
+const album2 = document.getElementById('album-2')
+play22.addEventListener('click',  () => {
+    artistBox.classList.add('hidden');
+    artistMusicBox.classList.add('hidden');
+    single.classList.add('hidden');
+    musicAlbuns.classList.remove('hidden');
+    album2.classList.remove('hidden');
+  
 })
