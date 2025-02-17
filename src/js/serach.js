2c00251f61b5b481a2d63162869bbf6d66b35f31
brunotxrs <elielsonbrunno@gmail.com>
2025-02-17 16:40:00 -0400

    feat: create one eventListener in moment has the mouse out!

--- src/js/serach.js
+++ src/js/serach.js
@@ -1,20 +1,20 @@
-const buscar = document.getElementById('buscar')
-const bc = document.getElementById('bc')
+const buscar = document.getElementById('buscar');
+const bc = document.getElementById('bc');
 const inputOuca = [
     document.getElementById('input'),
     document.getElementById('ouca')
 ]
 
 const back = document.getElementById('back');
-const digitar = document.getElementById('digitar')
-const outros = document.getElementById('outros')
-
+const digitar = document.getElementById('digitar');
+const outros = document.getElementById('outros');
+const navegar = document.getElementById('navegar');
 
 buscar.addEventListener('click', () => {
     bc.classList.add('ocult');
     inputOuca.forEach((element) => {
         element.classList.remove('ocult')
-    })
+    });
 });
 
 back.addEventListener('click', () => {
@@ -22,13 +22,19 @@ back.addEventListener('click', () => {
     outros.classList.add('ocult');
     inputOuca.forEach((element) => {
         element.classList.add('ocult')
-    })
+    });
 
-})
+});
 
 digitar.addEventListener('input', () => {
     outros.classList.remove('ocult');
-    inputOuca[1].classList.add('ocult')
+    inputOuca[1].classList.add('ocult');
+    navegar.classList.add('ocult');
 
+});
 
+digitar.addEventListener('mouseout', () => {
+    outros.classList.add('ocult');
+    inputOuca[1].classList.remove('ocult');
+    navegar.classList.remove('ocult');
 })
