36e2e2a107a7350976361b2b63571f4eef1b7178
brunotxrs <elielsonbrunno@gmail.com>
2025-02-18 21:22:29 -0400

    feat: create the function to API and show the artist after call of API!

--- src/js/serach.js
+++ src/js/serach.js
@@ -10,6 +10,7 @@ const digitar = document.getElementById('digitar');
 const outros = document.getElementById('outros');
 const navegar = document.getElementById('navegar');
 const clean = document.getElementById('clean');
+const container = document.getElementById('container')
 
 buscar.addEventListener('click', () => {
     bc.classList.add('ocult');
@@ -19,28 +20,68 @@ buscar.addEventListener('click', () => {
 });
 
 back.addEventListener('click', () => {
-    bc.classList.remove('ocult');
-    outros.classList.add('ocult');
-    inputOuca.forEach((element) => {
-        element.classList.add('ocult')
-    });
-
+    location.reload()
 });
 
-digitar.addEventListener('input', () => {
+function callApi(buscar){
+    const url = `http://localhost:3000/artistas?name_like=${buscar}`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+
+}
+
+// função de fazer o objeto da api aparecer 
+function displayResults(result){
     outros.classList.remove('ocult');
     inputOuca[1].classList.add('ocult');
     navegar.classList.add('ocult');
     clean.classList.remove('ocult')
-    clean.addEventListener('click', () => {
-        digitar.value = ''
-        clean.classList.add('ocult')
-    })
 
+    const name = document.getElementById('name');
+    const img = document.getElementById('img');
+
+    result.forEach(element => {
+        name.innerText = element.nome;
+        img.src = element.urlImg;
+    });
+
+    container.classList.remove('hidd');
+    
+}
+
+
+// quando houver ação de digita 
+document.addEventListener('input', () => {
+
+    const search = digitar.value.toLowerCase();
+
+    if(search === ''){
+        outros.classList.add('ocult');
+        inputOuca[1].classList.remove('ocult');
+        navegar.classList.remove('ocult');
+        clean.classList.add('ocult');
+        
+        return;
+    }
+
+    // chamando a api
+    callApi(search)
+
+    
 });
 
-digitar.addEventListener('mouseout', () => {
+// limpar o input apos clicar o botão de limpar 
+clean.addEventListener('click', () => {
+
+    digitar.value = ''
+    clean.classList.add('ocult');
+    container.style.display = 'none'
+
     outros.classList.add('ocult');
     inputOuca[1].classList.remove('ocult');
     navegar.classList.remove('ocult');
-})
+    clean.classList.add('ocult');
+
+});
