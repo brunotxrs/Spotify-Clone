9fea7a01d0c36850048fb2a3c386e81c4659c436
brunotxrs <elielsonbrunno@gmail.com>
2025-02-26 18:30:54 -0400

    feat: update the function to call index in version mobile!

--- src/js/serach.js
+++ src/js/serach.js
@@ -1,54 +1,36 @@
-const buscar = document.getElementById('buscar');
-const ind =  document.getElementById('ind')
-const pag3 = 'input.html'
-const pagHome = '../../index.html';
-const loading = document.getElementById('loader')
-buscar.addEventListener('click', () => {
-    location.href = pag3
+const buscar = document.getElementById("buscar");
+const ind = document.getElementById("ind");
+const pag3 = "input.html";
+const pagHome = "../../index.html";
+const loading = document.getElementById("loader");
+
+buscar.addEventListener("click", () => {
+    location.href = pag3;
 });
 
-function loadingShow(){
-    loading.classList.remove('hidden')
+function loadingShow() {
+    loading.classList.remove("hidden");
 }
 
-function loadingOcult(){
-    loading.classList.add('hidden')
+function loadingOcult() {
+    loading.classList.add("hidden");
 }
-function load() {
-    loadingShow()
 
+function load(callback) {
+    loadingShow();
     setTimeout(() => {
-        loadingOcult()
-    }, 3000)
+        loadingOcult();
+        if (callback) callback();
+    }, 2000); 
 }
-
-// Função para detectar pressionamento da tecla Backspace
-// document.addEventListener('pop', function(event) {
-//     ind.classList.add('hidden')    
-//     if (event.key === 'Backspace') {
-//         load()
-      
-//       // Redireciona para o index principal
-//       location.href = pagHome;
-      
-//     }
-// });
-
-// window.addEventListener('popstate', () => {
-//     ind.classList.add('hidden');        
-//     load();
-//     alert("ola")
-// });
-
 
 
 document.addEventListener("visibilitychange", () => {
     if (document.visibilityState === "hidden") {
-
-        alert("Você pressionou voltar ou mudou de aba!");
         ind.classList.add("hidden");        
-        load();
-        location.href = pagHome
+        load(() => {
+            location.href = pagHome;
+        });
     }
 });
 
