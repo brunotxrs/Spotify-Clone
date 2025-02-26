417016784f7235e79de599c11f4c589f1b771d07
brunotxrs <elielsonbrunno@gmail.com>
2025-02-26 17:56:29 -0400

    feat: make one function to call page Home first test!

--- src/js/serach.js
+++ src/js/serach.js
@@ -1,35 +1,35 @@
 const buscar = document.getElementById('buscar');
 const ind =  document.getElementById('ind')
 const pag3 = 'input.html'
-const pag1 = '/index.html'
+const pagHome = '../../index.html'
 const loading = document.getElementById('loader')
 buscar.addEventListener('click', () => {
     location.href = pag3
 });
 
-// function loadingShow(){
-//     loading.classList.remove('hidden')
-// }
+function loadingShow(){
+    loading.classList.remove('hidden')
+}
 
-// function loadingOcult(){
-//     loading.classList.add('hidden')
-// }
-// function load() {
-//     loadingShow()
+function loadingOcult(){
+    loading.classList.add('hidden')
+}
+function load() {
+    loadingShow()
 
-//     setTimeout(() => {
-//         loadingOcult()
-//     }, 3000)
-// }
+    setTimeout(() => {
+        loadingOcult()
+    }, 3000)
+}
 
 // Função para detectar pressionamento da tecla Backspace
-// document.addEventListener('keydown', function(event) {
-//     ind.classList.add('hidden')    
-//     if (event.key === 'Backspace') {
-//         load()
+document.addEventListener('keydown', function(event) {
+    ind.classList.add('hidden')    
+    if (event.key === 'Backspace') {
+        load()
       
-//       // Redireciona para o index principal
-//       location.assign(pag1);
+      // Redireciona para o index principal
+      location.href = pagHome;
       
-//     }
-// });
+    }
+});
