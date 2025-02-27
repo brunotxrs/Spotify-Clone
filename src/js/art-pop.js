bdc7da63525a3035a793ca7b9efec1db39c04ff0
brunotxrs <elielsonbrunno@gmail.com>
2025-02-27 10:35:18 -0400

    feat: update the function call index.html!

--- src/js/art-pop.js
+++ src/js/art-pop.js
@@ -1,5 +1,8 @@
 const home = document.getElementById('home');
 home.addEventListener('click', () => {
+    home.classList.add('homeOcult');
+    const homeAfterClick = document.getElementById('homeAfterClick');
+    homeAfterClick.classList.remove('homeOcult');
     const pageHome = '../../index.html'
-    location.href = pageHome
+    location.href = pageHome;
 })
